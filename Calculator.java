ef93ef489e443bea77c4acab9d75da17a2c39442
Ankit Gupta <44359589+ankitgupta1280@users.noreply.github.com>
2020-12-17 13:40:36 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+class Calculator{
+  public static int add(int num1,int num2){
+    return num1+num2;
+    }
+    public static void main(String []args){
+         int i=add(4,2);
+        system.out.println(i);
+    }
+    
+ }
+ 
