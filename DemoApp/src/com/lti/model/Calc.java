4a05b4bb2cb5aa57fd91391b7dcd3a19acebcef1
Ankit Gupta <44359589+ankitgupta1280@users.noreply.github.com>
2020-12-18 13:05:24 +0530

    added multiply method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -7,4 +7,7 @@ public class Calc {
 	public void subs (int num1,int num2) {
 		System.out.println("diffeerence is : "+(num1-num2));
 	}
+	public void multiply (int num1,int num2) {
+		System.out.println("product is : "+(num1*num2));
+	}
 }
