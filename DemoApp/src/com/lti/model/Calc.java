65eab21ccdc306f155daf1f70448b0199fe318b5
Ankit <ankit205123@gmail.com>
2020-12-18 15:49:29 +0530

    remove 1

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,8 +4,5 @@ public class Calc {
 	public void add(int num1, int num2) {
 		System.out.println("sum= " + (num1 + num2));
 	}
-	public void subs (int num1,int num2) {
-		System.out.println("diffeerence is : "+(num1-num2));
-	}
 	
 }
